61f73cf04cbb31811e10ffb914b281759aa57e1a
DracoXZ0 <londonosimon67873@gmail.com>
2023-05-08 21:03:26 -0500

    inicio de sesion con Google

--- login/lib/main.dart
+++ login/lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:login/homescreen/home.dart';
 import 'package:flutter/material.dart';
+import 'package:login/services/firebase_service.dart';
 
 void main() {
   runApp(const MaterialApp(
-    title: 'Logeo',
+    title: 'Navigation Basics',
     home: FirstRoute(),
   ));
 }
@@ -14,16 +15,34 @@ class FirstRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Simon'),
-        ),
+        appBar: AppBar(),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Center(
+                child: FutureBuilder(
+              future: FirebaseService.firebaseIni(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                          onPressed: () async {
+                            await FirebaseService.signInWithGoogle();
+                          },
+                          child: Text("Iniciar con Google"))
+                    ],
+                  );
+                } else {
+                  return const CircularProgressIndicator();
+                }
+              },
+            )),
+            Center(
               child: ElevatedButton(
-                child: const Text('Iniciar sesión con Google'),
+                child: const Text('Iniciar con Facebook'),
                 onPressed: () {
                   Navigator.push(
                     context,
@@ -34,18 +53,7 @@ class FirstRoute extends StatelessWidget {
             ),
             Center(
               child: ElevatedButton(
-                child: const Text('Iniciar sesión con Facebook'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const HomeScreen()),
-                  );
-                },
-              ),
-            ),
-            Center(
-              child: ElevatedButton(
-                child: const Text('Iniciar sesión con twitter'),
+                child: const Text('Iniciar con twitter'),
                 onPressed: () {
                   Navigator.push(
                     context,
--- login/test/widget_test.dart
+++ login/test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ejercicio_grid/main.dart';
+import 'package:login/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const FirstRoute());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
